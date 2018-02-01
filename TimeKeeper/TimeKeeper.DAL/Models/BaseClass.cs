07e2758edc0679f19a9fd97d90e6adcaec745425
BilVocke <muhamedbr6@gmail.com>
2018-02-01 09:34:34 +0100

    Added BaseClass

---
+++ TimeKeeper/TimeKeeper.DAL/Models/BaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.DAL.Models
+{
+    public abstract class BaseClass
+    {
+        public int Id { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool Deleted { get; set; }
+
+        public BaseClass()
+        {
+            CreatedBy = 0;
+            CreatedOn = DateTime.UtcNow;
+            Deleted = false;
+        }
+    }
+}
