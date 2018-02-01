ddbf36d7012f7cc397d21d7a43cc5f443e908d76
Emir Sabljica <emirsa@mistral.ba>
2018-02-01 16:12:11 +0100

    Customers and Access Models

---
+++ TimeKeeper/TimeKeeper.DAL/Models/Access.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.DAL.Models
+{
+	public class Access
+	{
+		
+		public int RoleId { get; set; }
+		public string Entity { get; set; }
+		public string Right { get; set; }
+		public string TeamFlag { get; set; }
+		public int PersonFlag { get; set; }
+		
+	}
+}
