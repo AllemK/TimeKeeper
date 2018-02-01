1652cbb18c8c5aab3aa7b76b9d6e189eba3d2518
BilVocke <muhamedbr6@gmail.com>
2018-02-01 14:52:43 +0100

    Added Employee and Role domain classes

---
+++ TimeKeeper/TimeKeeper.DAL/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.DAL.Models
+{
+    public enum Status
+    {
+        Active,
+
+    }
+
+    public class Employee:BaseClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Image { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public enum Status { get; set; }
+
+        public virtual Role Role { get; set; }
+
+    }   
+}
