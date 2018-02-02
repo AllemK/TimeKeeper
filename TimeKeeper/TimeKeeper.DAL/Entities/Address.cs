d7d7c64d6bd940b25a33218e222598ff5e91bd42
Emir Sabljica <emirsa@mistral.ba>
2018-02-02 11:07:56 +0100

    Added Classes Customers Projects and Addresses

---
+++ TimeKeeper/TimeKeeper.DAL/Entities/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.DAL.Entities
+{
+	public class Address
+	{
+		public string Road { get; set; }
+		public string Zip { get; set; }
+		public string City { get; set; }
+	}
+}
