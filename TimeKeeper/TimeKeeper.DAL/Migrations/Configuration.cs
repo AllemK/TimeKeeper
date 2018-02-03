2965452779662c38ac57a3b1b0a050063bdcdffd
BilVocke <muhamedbr6@gmail.com>
2018-02-03 14:10:28 +0100

    Modified Seed methond in Migrations config

--- TimeKeeper/TimeKeeper.DAL/Migrations/Configuration.cs
+++ TimeKeeper/TimeKeeper.DAL/Migrations/Configuration.cs
@@ -20,27 +20,92 @@ namespace TimeKeeper.DAL.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-            for (int i = 0; i < 5; i++)
+            base.Seed(context);
+
+            var customers = new List<Customer>();
+            var teams = new List<Team>();
+            var roles = new List<Role>();
+            var employees = new List<Employee>();
+            var projects = new List<Project>();
+
+            for (int i = 1; i < 15; i++)
             {
-                int j = 0;
-                Employee tmp = new Employee()
+                Address a = new Address
+                {
+                    City = "City" + i.ToString(),
+                    Road = "Road" + i.ToString(),
+                    Zip = i * 1000
+                };
+                customers.Add(new Customer
+                {
+                    Name = "Customer" + i.ToString(),
+                    Image = "ImgCust" + i.ToString(),
+                    Address = a,
+                    Contact = "Cont" + i.ToString(),
+                    Email = "mail" + i.ToString() + "@something.com",
+                    Monogram = "Monogram" + i.ToString(),
+                    Phone = "PhoneCust" + i.ToString(),
+                    Status = (i % 3) == 0 ? CustomerStatus.Client : CustomerStatus.Prospect
+                });
+            }
+
+            for (int i = 1; i < 4; i++)
+            {
+                teams.Add(new Team
+                {
+                    Id = "Team" + i.ToString(),
+                    Name = "Team" + i.ToString(),
+                    Description = "Desc" + i.ToString()
+                });
+            }
+
+            for (int i = 1; i < 6; i++)
+            {
+                roles.Add(new Role
+                {
+                    Id = "Role" + i.ToString(),
+                    Name = "Role" + i.ToString(),
+                    HourlyPrice = 30m,
+                    MonthlyPrice = 1200m
+                });
+            }
+
+            int x = 0;
+            for (int i = 1; i < 30; i++)
+            {
+                employees.Add(new Employee
                 {
                     FirstName = "Emp" + i.ToString(),
                     LastName = "Loy" + i.ToString(),
-                    Role = context.Role.Find("Rola" + i.ToString()),
-                    Birthday = DateTime.Now,
-                    Status = Status.Active,
-                    BeginDate = DateTime.Now
-                };
-                if (j > 3)
+                    Image = "ImgEmp" + i.ToString(),
+                    Email = "mail" + i.ToString() + "@mistral.ba",
+                    Phone = "PhoneEmp" + i.ToString(),
+                    Birthday = DateTime.MinValue.AddMonths(i),
+                    BeginDate = DateTime.UtcNow,
+                    Status = i % 3 == 0 ? Status.Trial : Status.Active,
+                    Role = roles[x++]
+                });
+                if (x == roles.Count)
                 {
-                    j = 0;
+                    x = 0;
                 }
-                tmp.Role = context.Role.Find("Rola" + j.ToString());
-                j++;
-                context.Employee.Add(tmp);
             }
 
+            x = 0;
+            foreach (var c in customers.Where(y=>y.Status==CustomerStatus.Client))
+            {
+                Project p = new Project
+                {
+                    Name = "Project" + (x+1).ToString(),
+                    Pricing = Pricing.FixedPrice,
+                    Description = "Desc"+(x+1).ToString(),
+                    StartDate= new DateTime(2017,x+2,x+5),
+                    ProjectStatus=ProjectStatus.InProgress,
+                    Customer = c,
+                    Team = teams[x++]
+                };
+                context.Project.AddOrUpdate(p);
+            }
         }
     }
 }
