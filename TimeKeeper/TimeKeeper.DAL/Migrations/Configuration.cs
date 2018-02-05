28ddbb4609a4418af09e8efcef3730d9acd3811a
Alem Kožljak <alemko@mistral.ba>
2018-02-05 09:36:54 +0100

    Edited Configuration.cs file

--- TimeKeeper/TimeKeeper.DAL/Migrations/Configuration.cs
+++ TimeKeeper/TimeKeeper.DAL/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace TimeKeeper.DAL.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TimeKeeper.DAL.Entities;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TimeKeeper.DAL.TimeKeeperContext>
     {
@@ -19,7 +20,26 @@ namespace TimeKeeper.DAL.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
 
-
+            for (int i = 0; i < 5; i++)
+             {
+                int j = 0;
+                Employee tmp = new Employee()
+                {
+                    FirstName = "Emp" + i.ToString(),
+                    LastName = "Loy" + i.ToString(),
+                    Role = context.Role.Find("Rola" + i.ToString()),
+                    Birthday = DateTime.Now,
+                    Status = Status.Active,
+                    BeginDate = DateTime.Now
+                 };
+                    if (j > 3)
+                        {
+                            j = 0;
+                        }
+                tmp.Role = context.Role.Find("Rola" + j.ToString());
+                j++;
+                context.Employee.Add(tmp);
+             }
         }
     }
 }
