b4f6d5d000c00200fcaa059b58f5ab9ecdde1b2a
Alem Kožljak <alemko@mistral.ba>
2018-02-14 08:52:59 +0100

    Added Projects controller

---
+++ TimeKeeper/TimeKeeper.API/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TimeKeeper.API.Models;
+using TimeKeeper.DAL.Entities;
+using TimeKeeper.DAL.Repository;
+
+namespace TimeKeeper.API.Controllers
+{
+    public class ProjectsController : BaseController
+    {
+        /*public IHttpActionResult Get()
+        {
+
+        }*/
+    }
+}
