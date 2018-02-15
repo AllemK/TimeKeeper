da40f07f5daf55b1c42651dd7bc11668f892734f
BilVocke <muhamedbr6@gmail.com>
2018-02-15 13:35:56 +0100

    Defined Utility Log and moved it to DAL project

--- TimeKeeper/TimeKeeper.API/Controllers/EmployeesController.cs
+++ TimeKeeper/TimeKeeper.API/Controllers/EmployeesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TimeKeeper.DAL;
 using TimeKeeper.DAL.Entities;
 
 namespace TimeKeeper.API.Controllers
@@ -13,6 +14,7 @@ namespace TimeKeeper.API.Controllers
         public IHttpActionResult Get()
         {
             var list = TimeKeeperUnit.Employees.Get().ToList().Select(x => TimeKeeperFactory.Create(x)).ToList();
+            Utility.Log("returned all records for employees", "INFO");
             return Ok(list);
         }
 
@@ -21,10 +23,12 @@ namespace TimeKeeper.API.Controllers
             Employee emp = TimeKeeperUnit.Employees.Get(id);
             if (emp == null)
             {
+                //Utility.Log(ex.Message, "ERROR", ex);
                 return NotFound();
             }
             else
             {
+                Utility.Log($"returned record for employee {emp.FullName}", "INFO");
                 return Ok(TimeKeeperFactory.Create(emp));
             }
         }
@@ -49,9 +53,13 @@ namespace TimeKeeper.API.Controllers
         {
             try
             {
-                if (TimeKeeperUnit.Employees.Get(id) == null) return NotFound();
+                if (TimeKeeperUnit.Employees.Get(id) == null)
+                {
+                    return NotFound();
+                }
                 TimeKeeperUnit.Employees.Update(emp, id);
                 TimeKeeperUnit.Save();
+                Utility.Log($"returned record for employee {emp.FullName}", "INFO");
                 return Ok(emp);
             }
             catch (Exception ex)
@@ -65,9 +73,13 @@ namespace TimeKeeper.API.Controllers
             try
             {
                 Employee emp = TimeKeeperUnit.Employees.Get(id);
-                if (emp == null) return NotFound();
+                if (emp == null)
+                {
+                    return NotFound();
+                }
                 TimeKeeperUnit.Employees.Delete(emp);
                 TimeKeeperUnit.Save();
+                Utility.Log($"returned record for employee {emp.FullName}", "INFO");
                 return Ok();
             }
             catch (Exception ex)
