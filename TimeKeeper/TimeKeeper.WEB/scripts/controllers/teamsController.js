8171af01dd086c2e7a20bf5aae2ec0adb830e076
Emir Sabljica <emirsa@mistral.ba>
2018-04-04 09:35:17 +0200

    some small changes

--- TimeKeeper/TimeKeeper.WEB/scripts/controllers/teamsController.js
+++ TimeKeeper/TimeKeeper.WEB/scripts/controllers/teamsController.js
@@ -1,20 +1,18 @@
 (function(){
     var app = angular.module("timeKeeper");
 
-    app.controller("teamsController", ["$scope", "dataService", "timeConfig", "$uibModal","$location", function($scope, dataService, timeConfig, $uibModal, $location) {
+    app.controller("teamsController", ["$scope", "dataService", "timeConfig", "$location", function($scope, dataService, timeConfig, $location) {
 
         $scope.currentPage = 0;
-        $scope.message = "Wait...";
-        dataService.list("teams", function(data, headers){
-            $scope.page = angular.fromJson(headers('Pagination'));
-            $scope.totalItems = $scope.page.totalItems;
-            $scope.message = "";
-            $scope.teams = data;
-            console.log(data);
-        });
 
-
-
+        function listTeams() {
+            dataService.list("teams", function (data, headers) {
+                $scope.page = angular.fromJson(headers('Pagination'));
+                $scope.totalItems = $scope.page.totalItems;
+                $scope.teams = data;
+            });
+        }
+        listTeams();
         $scope.pageChanged = function() {
             dataService.list("teams?" +"page="+($scope.currentPage-1), function(data, headers){
                 $scope.projects = data;
@@ -22,31 +20,48 @@
             $log.log('Page changed to: ' + $scope.currentPage);
         };
 
-        function listTeams() {
-            dataService.list("teamss", function (data) {
-                $scope.message = "";
-                $scope.teams = data;
-            });
-        }
+        dataService.list("employees?all", function(data){
+            $scope.employees=data;
+        });
 
-        listTeams();
+        dataService.list("roles", function(data){
+            $scope.roles=data;
+        });
 
-        $scope.$on("teamsUpdated", function(event){
+        $scope.$on("teamsUpdated",function(event){
             listTeams();
         });
 
+        $scope.clicked = false;
+        $scope.select = function(data) {
+            $scope.team = data;
+            $scope.clicked = true;
+        };
+
     }]);
-    app.controller("teaController", ["$scope", "$uibModal", "dataService", "toaster" , function($scope, $uibModal, dataService, toaster) {
-        var $tea = this;
+    app.controller("teaController", ["$scope", "$uibModal", "dataService", function($scope, $uibModal, dataService) {
+        $scope.addMember = function(data){
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'views/team/newMember.html',
+                controller: 'tmModalCtrl',
+                resolve:{
+                    team: function(){
+                        return data;
+                    }
+                }
+            });
+        };
 
         $scope.edit = function (data) {
             var modalInstance = $uibModal.open({
                 animation: true,
                 ariaLabelledBy: 'modal-title',
                 ariaDescribedBy: 'modal-body',
-                templateUrl: 'views/Team/tmModal.html',
+                templateUrl: 'views/team/tmModal.html',
                 controller: 'tmModalCtrl',
-                controllerAs: '$tea',
                 resolve: {
                     team: function () {
                         return data;
@@ -60,23 +75,7 @@
                 animation: true,
                 ariaLabelledBy: 'modal-title',
                 ariaDescribedBy: 'modal-body',
-                templateUrl: 'views/Team/newTeam.html',
-                controller: 'tmModalCtrl',
-                controllerAs: '$tea',
-                resolve: {
-                    team: function () {
-                        return data;
-                    }
-                }
-            })
-        };
-
-        $scope.view = function(data){
-            var modalInstance = $uibModal.open({
-                animation: true,
-                ariaLabelledBy: 'modal-title',
-                ariaDescribedBy: 'modal-body',
-                templateUrl: 'views/Team/teamProfile.html',
+                templateUrl: 'views/team/newTeam.html',
                 controller: 'tmModalCtrl',
                 controllerAs: '$tea',
                 resolve: {
@@ -89,85 +88,55 @@
 
         $scope.delete = function(team){
             dataService.delete("teams", team.id, function(data){
-                window.alert("Data deleted!");
             })
         };
+    }]);
 
+    app.controller('tmModalCtrl', ['$uibModalInstance', '$scope', 'dataService', 'team',
+        function ($uibModalInstance, $scope, dataService, team) {
+            $scope.team = team;
 
+            dataService.list("roles",function(data){
+                $scope.roles = data;
+            });
 
-        $scope.clickwar = function(team){
-            swal({
-                title: team.name,
-                text: "Are you sure you want to delete this team?",
-                type: "warning",
-                showCancelButton: true,
-                customClass: "sweetClass",
-                confirmButtonColor: "red",
-                confirmButtonText: "Yes, sure",
-                cancelButtonColor: "",
-                cancelButtonText: "No, not ever!",
-                closeOnConfirm: false,
-                closeOnCancel: true
-            },
-                function(isConfirm){
-                    if(isConfirm){
-                        dataService.delete("teams", team.id, function(){
-                            $scope.$emit("teamsUpdated");
-                        });
-                        console.log("Team deleted");
-                        swal.close();
-                    }
+            dataService.list("employees?all", function(data){
+                $scope.employees = data;
+            });
+
+            dataService.list("projects?all", function(data){
+                $scope.projects = data;
+            });
+
+            $scope.save = function (team) {
+                dataService.update("teams", team.id, team, function(data){
+                    $scope.$emit("teamsUpdated");
                 });
-        };
-        $scope.$on("teamsUpdated", function(event){
-            $scope.$emit("teamsUpdated");
-        })
+            };
+            $scope.saveNew = function (team) {
+                dataService.insert("teams", team, function(data){
+                    $scope.$emit("teamsUpdated");
+                });
+            };
 
+            $scope.saveMember = function(team){
+                console.log()
+                var m = {
+                    id:0,
+                    hours:team.engagements.member.hours,
+                    team:{id:team.id},
+                    employee:member.employee,
+                    role:member.role
+                };
+                //dataService.insert("members",member,function(data){
+                console.log($scope.team.engagements);
+                //});
 
-    }]);
+                $uibModalInstance.close();
+            };
 
-    app.controller('tmModalCtrl', ['$uibModalInstance', '$scope', 'dataService', 'team', function ($uibModalInstance, $scope, dataService, team) {
-        var $tea = this;
-        $scope.team = team;
-
-        dataService.list("employees?all", function(data){
-            $scope.employees = data;
-        });
-
-        dataService.list("projects?all", function(data){
-            $scope.projects = data;
-        });
-
-
-
-        $scope.save = function (team) {
-            dataService.update("teams", team.id, team, function(data){
-                window.alert("Data updated!");
-            });
-        };
-        $scope.saveNew = function(team){
-            dataService.insert("teams",team,function(data){
-                window.alert("Data updated!");
-            });
-            $scope.$emit("teamsUpdated");
-            $uibModalInstance.close();
-        };
-
-        /* delete employee in team */
-
-        /*$scope.delEmp = function(person){
-            dataService.delEmp("teams", person.id, function(data){
-                window.alert("Data deleted!");
-            })
-        };*/
-
-        /*$scope.delEmp = function(team) {
-            $scope.people.teams[team].deleted = true;
-        };*/
-
-
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss();
-        };
-    }]);
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss();
+            };
+        }]);
 }());
